eccca4d1712e11682988de0cf2862053cac19bd0
alemohamad <alejandro.mohamad@gmail.com>
2014-01-10 01:51:29 -0300

    All views were updated to BS3.

--- application/views/formulize/file.php
+++ application/views/formulize/file.php
@@ -1,14 +1,19 @@
 <?php header("Content-type: text/html; charset=utf-8"); ?>
-<div class="control-group">
-  <label class="control-label" for="<?php echo $item['varname']; ?>"><?php echo $item['name']; ?></label>
-  <div class="controls">
-    <input type="file" class="input-xlarge" id="<?php echo $item['varname']; ?>" name="<?php echo $item['varname']; ?>">
-    <input name="prev_<?php echo $item['varname']; ?>" type="hidden" value="<?php echo $item['value']; ?>" id="<?php echo $item['varname']; ?>-file">
-    <span class="help-block"><small><b>Formats:</b> <?php echo $item['elements']['formats']; ?>. <b>Max size:</b> <?php echo $item['elements']['size']; ?>.</small></span>
-    <?php if(!empty($item['value'])): ?>
-    <span class="help-block"><label><input type="checkbox" id="<?php echo $item['varname']; ?>-file-ck" style="width: 11px; height: 11px;"> <small>Remove file</small></label></span>
-    <?php endif; ?>
-  </div>
+<div class="form-group">
+	<label for="<?php echo $item['varname']; ?>" class="col-sm-2 control-label"><?php echo $item['name']; ?></label>
+	<div class="col-sm-10">
+	    <input type="file" id="<?php echo $item['varname']; ?>" name="<?php echo $item['varname']; ?>">
+	    <input name="prev_<?php echo $item['varname']; ?>" type="hidden" value="<?php echo $item['value']; ?>" id="<?php echo $item['varname']; ?>-file">
+	    <span class="help-block small"><strong>Formats:</strong> <?php echo $item['elements']['formats']; ?>. <strong>Max size:</strong> <?php echo $item['elements']['size']; ?>.
+	    <?php if(!empty($item['value'])): ?>
+	    <br><label class="checkbox-inline"><input type="checkbox" id="<?php echo $item['varname']; ?>-file-ck"> Remove file
+		<? if( preg_match("/picture/i", $item['varname']) ):?>
+		- <a href="<?=site_url( picture_path('assets/uploads/' . $item['value'], 'l') )?>" target="_blank" rel="popover" data-content="<img src='<?=site_url( picture_path('assets/uploads/' . $item['value'], 's') )?>' alt='Image preview' style='max-width: 250px;'>" data-html="true" data-trigger="hover" data-delay="300"><span class="glyphicon glyphicon-picture"></span></a>
+		<? endif;?>
+        </label>
+	    <?php endif; ?>
+		</span>
+	</div>
 </div>
 
 <script>
